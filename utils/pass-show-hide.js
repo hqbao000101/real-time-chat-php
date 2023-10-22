fdee2e74d8c6f24a4c5642e17311287319f0c87f
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-10-22 21:36:20 +0700

    js-signup-show-hide-pass

--- utils/pass-show-hide.js
+++ utils/pass-show-hide.js
@@ -1,1 +1,15 @@
-console.log("Password toggle...");
+const signUpPass = document.getElementById("sign-up-pass");
+const showPass = document.getElementById("sign-up-eye");
+const hidePass = document.getElementById("sign-up-eye-slash");
+
+showPass.onclick = () => {
+  signUpPass.setAttribute("type", "text");
+  showPass.style.visibility = "hidden";
+  hidePass.style.visibility = "visible";
+};
+
+hidePass.onclick = () => {
+  signUpPass.setAttribute("type", "password");
+  showPass.style.visibility = "visible";
+  hidePass.style.visibility = "hidden";
+};
